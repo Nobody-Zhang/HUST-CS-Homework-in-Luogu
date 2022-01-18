71823ca96b19b875bc4c060f571e3372aa8f1811
“ZGB” <45116315+Nobody-Zhang@users.noreply.github.com>
2022-01-18 17:38:39 +0800

    1/17

--- 【1-2】分支/P1055 [NOIP2008 普及组] ISBN 号码.cpp
+++ 【1-2】分支/P1055 [NOIP2008 普及组] ISBN 号码.cpp
@@ -1,8 +1,34 @@
+#include<stdlib.h>
 #include<stdio.h>
-//x-xxx-xxxxx-x
+#include<string.h>
 int main()
 {
-	char a[101];
-	gets(a);
-	
+	char s[15];
+	int num[13],k=0;
+	memset(num,0,sizeof(num));
+	gets(s);
+	for(int i=0;s[i];i++)
+	{
+		if(s[i]!='-')
+		{
+			if(s[i]=='X')
+				num[++k]=10;
+			else num[++k]=s[i]-'0';
+		}
+	}
+	int ch=0;
+	for(int i=1;i<=9;i++)
+	{
+		ch=ch+i*num[i];
+		ch%=11;
+	}
+	if(ch==num[k]) printf("Right");
+	else
+	{
+		for(int i=0;s[i+1];i++)
+			putchar(s[i]);
+		if(num[k]==10) printf("X");
+		else printf("%d",ch);
+	}
+	return 0;
 }
